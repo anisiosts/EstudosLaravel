41c1aff4f25246a80a5c4ca8bb9dbb05e6f8be8a
anisiosts <bsantosanisio@gmail.com>
2018-07-09 20:24:04 -0300

    Finalmente resolvi o erro, era no formulario.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Post;
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Post;
+use Session;
 
 class PostController extends Controller
 {
@@ -15,7 +18,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::orderBy('id', 'desc')->paginate(10);
+        return view('posts.index')->withPosts($posts);
     }
 
     /**
@@ -25,7 +29,7 @@ class PostController extends Controller
      */
     public function create()
     {
-         return view('posts.create');
+        return view('posts.create');
     }
 
     /**
@@ -35,21 +39,24 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {   
-        
-        
+    {
+        // validate the data
         $this->validate($request, array(
-            'title' => 'required|max:255',
-            'body' => 'required'
-        )); 
+                'title' => 'required|max:255',
+                'body'  => 'required'
+            ));
 
-        
+        // store in the database
         $post = new Post;
+
         $post->title = $request->title;
         $post->body = $request->body;
+
         $post->save();
 
-        return redirect()->route('posts.show',$post->id);
+        Session::flash('success', 'The blog post was successfully save!');
+
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
@@ -60,7 +67,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.show')->withPost($post);
     }
 
     /**
@@ -71,7 +79,10 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        // find the post in the database and save as a var
+        $post = Post::find($id);
+        // return the view and pass in the var we previously created
+        return view('posts.edit')->withPost($post);
     }
 
     /**
@@ -83,7 +94,24 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // Validate the data
+        $this->validate($request, array(
+                'title' => 'required|max:255',
+                'body'  => 'required'
+            ));
+        // Save the data to the database
+        $post = Post::find($id);
+
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        $post->save();
+
+        // set flash data with success message
+        Session::flash('success', 'This post was successfully saved.');
+
+        // redirect with flash data to posts.show
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
@@ -94,6 +122,11 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+
+        $post->delete();
+
+        Session::flash('success', 'The post was successfully deleted.');
+        return redirect()->route('posts.index');
     }
 }
